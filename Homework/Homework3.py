0909f071c40f7be38ed1331796f00566414717c3
anisko13 <anisochka12341@gmail.com>
2021-08-29 18:44:13 +0300

    Homework3

---
+++ Homework/Homework3.py
@@ -0,0 +1,8 @@
+x = int(input('Write a number '))
+if int(x) <= 0:
+    print(x)
+elif x > 20 or x < 20
+    print ('Number is bigger than 20') or ('Number is less than 20'))
+else:
+    print('Wrong number')
+
