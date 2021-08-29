bad1a5a534246061c9fea989befe73868f1e8159
Анна Прокопчук <89098906+anisko13@users.noreply.github.com>
2021-08-29 18:51:11 +0300

    Add files via upload

---
+++ Hw3.py
@@ -0,0 +1,15 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a, b, c = 24, -100, 100
+>>> max (a, b, c)
+100
+>>> if a > b and a > c:
+	print ('a')
+elif b > a and b > c:
+        print ('b')
+elif c > a and c > b:
+        print ('c')
+
+        
+c
+>>> 
